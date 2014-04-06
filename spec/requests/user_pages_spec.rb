2be41a6f94f8698731eb13eefdbd9cfc0985088b
Hideyuki Konishi <nishinohyde@gmail.com>
2014-04-06 20:07:06 +0900

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe "User pages" do
@@ -13,8 +14,28 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
+    let(:submit) { "Create my account" }
+
     it { expect(page).to have_selector('h1', text: 'Sign up') }
     it { expect(page).to have_selector('title', text: full_title('Sign up')) }
+
+    describe "正しくない情報を与えた時" do
+      it "userが作られない" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "正しい情報を与えた時" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      it "userが作られる" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
 
   end
 
